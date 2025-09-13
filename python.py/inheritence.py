a88f45042f3cd10b31cfcc3f5ff2d7382d1f8732
Venkatesh Narala <venkateshnarla66@gmail.com>
2025-09-13 09:42:26 -0400

    add new data

--- python.py/inheritence.py
+++ python.py/inheritence.py
@@ -1,15 +1,22 @@
-# class parent:
-#     def output(self):
-#         print('I am parent')
-# class child(parent):
-#     def outputc(self):
-#         print('i am child')
 
-# c1=child()
-# c1.output()
-# c1.outputc()
-# p=parent()
-# p.output()
+
+#single inheritence 
+
+class parent:
+    def output(self):
+        print('I am parent')
+class child(parent):
+    def outputc(self):
+        print('i am child')
+
+c1=child()
+c1.output()
+c1.outputc()
+p=parent()
+p.output()
+
+
+#inheritence concept multilevel inheritence 
 
 class grandfather:
     def outputgf(self):
@@ -25,3 +32,11 @@ c1=child()
 c1.outputc()
 c1.outputp()
 c1.outputgf()
+
+
+#for using _ this print second value
+
+for d in [{1:'one'},{2:'two'}]:
+    for _,y in d.items():
+        print(y) # only using second value
+
